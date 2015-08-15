ebd222e40d112dd8135d9494bb06d60a43565cc4
asi1024 <akifumiimanishi@yahoo.co.jp>
2015-08-15 20:54:07 +0900

    Add template

---
+++ Template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define REP(i,n) for(int i=0;i<(int)(n);i++)
+#define ALL(x) (x).begin(),(x).end()
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+const int INF = 1e9;
+const ld eps = 1e-9, pi = acos(-1.0);
+
+int main() {
+
+  return 0;
+}
