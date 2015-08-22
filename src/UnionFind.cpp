c01ad3ce4daf86d6d1ee4ee1a7ca146899293d10
asi1024 <akifumiimanishi@yahoo.co.jp>
2015-08-23 07:09:56 +0900

    Add UnionFind

---
+++ src/UnionFind.cpp
@@ -0,0 +1,15 @@
+struct UnionFind {
+  vector<int> parent;
+  UnionFind (int n) : parent(n, -1) {}
+  int root(int x) {
+    return parent[x] < 0 ? x : parent[x] = root(parent[x]);
+  }
+  bool merge(int x, int y) {
+    x = root(x); y = root(y);
+    if (x == y) return false;
+    if (parent[y] < parent[x]) swap(x, y);
+    if (parent[x] == parent[y]) --parent[x];
+    parent[y] = x;
+    return true;
+  }
+};
