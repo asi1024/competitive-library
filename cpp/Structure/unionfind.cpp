5c9eae5c08534484de4b3cba41731537834db8d1
asi1024 <akifumiimanishi@yahoo.co.jp>
2015-11-03 04:28:48 +0900

    fix unionfind

--- cpp/Structure/unionfind.cpp
+++ cpp/Structure/unionfind.cpp
@@ -2,18 +2,33 @@
 
 #include "../util.hpp"
 
+// Verified : AOJ DSL_1_A (Disjoint Set: Union Find Tree)
+
 struct UnionFind {
-  vector<int> parent;
-  UnionFind (int n) : parent(n, -1) {}
-  int root(int x) {
-    return parent[x] < 0 ? x : parent[x] = root(parent[x]);
-  }
-  bool merge(int x, int y) {
+  vector<int> p;
+  UnionFind (int n) : p(n, -1) {}
+  int root(int x) { return p[x] < 0 ? x : p[x] = root(p[x]); }
+  bool same(int x, int y) { return root(x) == root(y); }
+  bool unite(int x, int y) {
     x = root(x); y = root(y);
     if (x == y) return false;
-    if (parent[y] < parent[x]) swap(x, y);
-    if (parent[x] == parent[y]) --parent[x];
-    parent[y] = x;
+    if (p[y] < p[x]) swap(x, y);
+    if (p[x] == p[y]) --p[x];
+    p[y] = x;
     return true;
   }
 };
+
+/*
+int main() {
+  int n, q, com, x, y;
+  cin >> n >> q;
+  UnionFind uf(n);
+  REP(i,q) {
+    cin >> com >> x >> y;
+    if (com) cout << uf.same(x, y) << endl;
+    else uf.unite(x, y);
+  }
+  return 0;
+}
+*/
