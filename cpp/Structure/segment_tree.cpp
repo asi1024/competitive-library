8e3791acdbadcd3ba4102c3c9a42c0618002d1ed
asi1024 <akifumiimanishi@yahoo.co.jp>
2015-12-29 08:13:17 +0900

    fix segment tree

--- cpp/Structure/segment_tree.cpp
+++ cpp/Structure/segment_tree.cpp
@@ -4,41 +4,33 @@
 
 // Verified : AOJ DSL_2_A (Range Minimum Query)
 
-struct Data {
-  int num;
-  Data() : num(0x7FFFFFFF) {;}
-  Data(int n) : num(n) {;}
-};
-
-inline Data Merge(Data left, Data right) {
-  return Data(left.num < right.num ? left.num : right.num);
-}
-
+template<typename T>
 class SegmentTree {
+  using func_t = function<T(T, T)>;
 private:
-  int n; vector<Data> data;
-  Data sub(int fr, int to, int node, int la, int ra) {
-    if (ra<=fr || to<=la) return Data();
+  int n; T id; vector<T> data; func_t merge;
+  T sub(int fr, int to, int node, int la, int ra) {
+    if (ra<=fr || to<=la) return id;
     if (fr<=la && ra<=to) return data[node];
-    Data vl = sub(fr, to, (node-n)*2+0, la, (la+ra)/2);
-    Data vr = sub(fr, to, (node-n)*2+1, (la+ra)/2, ra);
-    return Merge(vl, vr);
+    T vl = sub(fr, to, (node-n)*2+0, la, (la+ra)/2);
+    T vr = sub(fr, to, (node-n)*2+1, (la+ra)/2, ra);
+    return merge(vl, vr);
   }
 public:
-  SegmentTree(int N) : n(N) {
+  SegmentTree(int N, T id, func_t f) : n(N), id(id), merge(f) {
     while (n != (n & -n)) n += n & -n;
-    data.assign(n * 2, Data());
+    data.assign(n * 2, id);
   }
-  void update(int pos, Data value) {
+  void update(int pos, T value) {
     data[pos] = value;
     while (pos < 2*n-1) {
       int l = pos, r = pos^1;
       if (l > r) swap(l, r);
       pos = pos / 2 + n;
-      data[pos] = Merge(data[l], data[r]);
+      data[pos] = merge(data[l], data[r]);
     }
   }
-  Data find(int fr, int to) {
+  T find(int fr, int to) {
     return sub(fr, to, 2*n-2, 0, n);
   }
 };
@@ -47,11 +39,11 @@ public:
 int main() {
   int n, q, com, x, y;
   cin >> n >> q;
-  SegmentTree seg(n);
+  SegmentTree<int> seg(n, 0x7FFFFFFF, [](int a, int b){return min(a, b);});
   while (q--) {
     cin >> com >> x >> y;
-    if (com) cout << seg.find(x, y+1).num << endl;
-    else seg.update(x, Data(y));
+    if (com) cout << seg.find(x, y+1) << endl;
+    else seg.update(x, y);
   }
   return 0;
 }
