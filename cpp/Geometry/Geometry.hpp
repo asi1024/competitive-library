3c002c501e6ae8a80847efc63601a1824f348545
asi1024 <akifumiimanishi@yahoo.co.jp>
2016-03-04 14:29:35 +0900

    fix geometry.hpp

--- cpp/Geometry/Geometry.hpp
+++ cpp/Geometry/Geometry.hpp
@@ -11,9 +11,23 @@ const ld eps = 1e-9;
 ld dot(Point a, Point b) { return real(conj(a) * b); }
 ld cross(Point a, Point b) { return imag(conj(a) * b); }
 
-struct Line { Point a, b; };
-struct Segment { Point a, b; };
-struct Circle { Point p; ld r; };
+struct Line {
+  Point a, b;
+  Line (Point p, Point q) : a(p), b(q) {;}
+};
+
+struct Segment {
+  Point a, b;
+  Segment (Point p, Point q) : a(p), b(q) {;}
+};
+
+struct Circle {
+  Point p; ld r;
+  Circle (Point a, ld b) : p(a), r(b) {;}
+};
+
+Line to_line(Segment s) { return Line(s.a, s.b); }
+Segment to_segment(Line l) { return Segment(l.a, l.b); }
 
 // counter clockwise
 int ccw (Point a, Point b, Point c) {
