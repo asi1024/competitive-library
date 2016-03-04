4c6685625054603f171930a2c1c877673fe4ff26
asi1024 <akifumiimanishi@yahoo.co.jp>
2016-03-04 12:02:11 +0900

    add some libs in spec

--- cpp/spec.cpp
+++ cpp/spec.cpp
@@ -2,6 +2,7 @@
 #include "Structure/segment_tree.cpp"
 #include "Structure/fenwick_tree.cpp"
 #include "Structure/starry_sky_tree.cpp"
+
 #include "Graph/Graph.hpp"
 #include "Graph/dijkstra.cpp"
 #include "Graph/spfa.cpp"
@@ -9,4 +10,9 @@
 #include "Graph/max_flow.cpp"
 #include "Graph/min_cost_flow.cpp"
 #include "Graph/bipartite_matching.cpp"
+
+#include "Math/mod.cpp"
+
+#include "Geometry/Geometry.hpp"
+
 #include "template.cpp"
