07a47fa04f5472ed04380d32beda4d128e075d02
asi1024 <akifumiimanishi@yahoo.co.jp>
2016-03-07 15:23:01 +0900

    add rep class deprecated rep macro

--- cpp/template.cpp
+++ cpp/template.cpp
@@ -1,9 +1,23 @@
 #include <bits/stdc++.h>
 
-#define REP(i,n) for(int i=0;i<(int)(n);i++)
-#define ALL(x) (x).begin(),(x).end()
-
 using namespace std;
+
+class rep {
+  struct repi {
+    size_t it;
+    repi() : it(0) { }
+    repi(size_t n) : it(n) {}
+    bool operator==(const repi &rhs) const { return it == rhs.it; }
+    bool operator!=(const repi &rhs) const { return it != rhs.it; }
+    size_t& operator*() { return it; }
+    repi& operator++() { ++it; return *this; }
+  };
+  size_t i;
+public:
+  rep(size_t n) : i(n) { }
+  repi begin() const { return 0; }
+  repi end() const { return i; }
+};
 
 using ll = long long;
 using ld = long double;
