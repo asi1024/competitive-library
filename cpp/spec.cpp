6d0fd3e96d9d25370c021aeac8d51e8616e95bf9
asi1024 <akifumiimanishi@yahoo.co.jp>
2016-03-20 14:45:07 +0900

    add recession-analysis

--- cpp/spec.cpp
+++ cpp/spec.cpp
@@ -12,6 +12,7 @@
 #include "Graph/min_cost_flow.cpp"
 #include "Graph/bipartite_matching.cpp"
 #include "Graph/lca.cpp"
+#include "Graph/recession.cpp"
 
 #include "Math/mod.cpp"
 #include "Math/eratosthenes.cpp"
