b2f984543bb5590fdc55770e8ae22db45e7dd333
asi1024 <imanishi@fos.kuis.kyoto-u.ac.jp>
2016-05-31 13:18:21 +0900

    cosmetic

--- cpp/Structure/unionfind.cpp
+++ cpp/Structure/unionfind.cpp
@@ -6,8 +6,12 @@ class UnionFind {
   vector<int> p;
 public:
   UnionFind (int n) : p(n, -1) {}
-  int root(int x) { return p[x] < 0 ? x : p[x] = root(p[x]); }
-  bool same(int x, int y) { return root(x) == root(y); }
+  int root(int x) {
+    return p[x] < 0 ? x : p[x] = root(p[x]);
+  }
+  bool same(int x, int y) {
+    return root(x) == root(y);
+  }
   bool unite(int x, int y) {
     x = root(x); y = root(y);
     if (x == y) return false;
