322630719c1d5aa74516be4cb47b2b1fb5d9a693
asi1024 <imanishi@fos.kuis.kyoto-u.ac.jp>
2016-06-02 17:46:03 +0900

    fix allocation of index -- segment_tree

--- cpp/Structure/segment_tree.cpp
+++ cpp/Structure/segment_tree.cpp
@@ -5,30 +5,34 @@
 template <typename T>
 class SegmentTree {
   using func_t = function<T(T, T)>;
-  int n; T id; vector<T> data; func_t merge;
+  const int n;
+  const T id;
+  func_t merge;
+  vector<T> data;
   T sub(int fr, int to, int node, int la, int ra) {
-    if (ra<=fr || to<=la) return id;
-    if (fr<=la && ra<=to) return data[node];
-    T vl = sub(fr, to, (node-n)*2+0, la, (la+ra)/2);
-    T vr = sub(fr, to, (node-n)*2+1, (la+ra)/2, ra);
+    if (ra <= fr || to <= la) return id;
+    if (fr <= la && ra <= to) return data[node];
+    T vl = sub(fr, to, node * 2 + 0, la, (la + ra) / 2);
+    T vr = sub(fr, to, node * 2 + 1, (la + ra) / 2, ra);
     return merge(vl, vr);
   }
-public:
-  SegmentTree(int N, T id, func_t f) : n(N), id(id), merge(f) {
-    while (n != (n & -n)) n += n & -n;
-    data.assign(n * 2, id);
+  int size(int n) {
+    return n == 1 ? n : size((n + 1) / 2) * 2;
   }
-  void update(int pos, T value) {
-    data[pos] = value;
-    while (pos < 2*n-1) {
-      int l = pos, r = pos^1;
-      if (l > r) swap(l, r);
-      pos = pos / 2 + n;
-      data[pos] = merge(data[l], data[r]);
+public:
+  SegmentTree(int n, T id, func_t merge) :
+    n(size(n)), id(id), merge(merge), data(n * 2, id) {}
+  void update(int p, T val) {
+    assert (0 <= p && p < n);
+    p += n;
+    data[p] = val;
+    while (p /= 2) {
+      int l = p * 2, r = p * 2 + 1;
+      data[p] = merge(data[l], data[r]);
     }
   }
   T find(int fr, int to) {
-    return sub(fr, to, 2*n-2, 0, n);
+    return sub(fr, to, 1, 0, n);
   }
 };
 
