8853c90f27a5cee2ec192e6384024bb0e084cdba
asi1024 <imanishi@fos.kuis.kyoto-u.ac.jp>
2016-09-04 22:17:56 +0900

    rename member function of fenwicktree : update -> add

--- src/Structure/fenwick_tree.cpp
+++ src/Structure/fenwick_tree.cpp
@@ -8,31 +8,13 @@ class FenwickTree {
   vector<T> data;
 public:
   FenwickTree(int n) : n(n), data(n, 0) {}
-  void update(int i, T value) {
+  void add(int i, T value) {
     for (; i < n; i |= i+1) data[i] += value;
   }
   T sum(int i) {
     T s = 0;
     for (; i >= 0; i = (i&(i+1))-1) s += data[i];
     return s;
-  }
-};
-
-template <typename T>
-class RARS {
-  FenwickTree<T> bit1, bit2;
-public:
-  RARS(int n) : bit1(n+1), bit2(n+1) {}
-  void add(int l, int r, T val) {
-    bit1.update(l, -val * l);
-    bit1.update(r,  val * r);
-    bit2.update(l,  val);
-    bit2.update(r, -val);
-  }
-  T sum(int l, int r) {
-    T s1 = bit1.sum(r) + bit2.sum(r) * r;
-    T s2 = bit1.sum(l) + bit2.sum(l) * l;
-    return s1 - s2;
   }
 };
 
@@ -45,7 +27,7 @@ int main() {
   while (q--) {
     cin >> com >> x >> y;
     if (com) cout << bit.sum(y) - bit.sum(x-1) << endl;
-    else bit.update(x, y);
+    else bit.add(x, y);
   }
   return 0;
 }
