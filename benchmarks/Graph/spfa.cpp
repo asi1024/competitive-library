7ba127a0f641318cb1ec1dc5558690ebe9228f7d
asi1024 <imanishi@fos.kuis.kyoto-u.ac.jp>
2016-09-04 23:26:00 +0900

    fix limitation of benchmarks/spfa

--- benchmarks/Graph/spfa.cpp
+++ benchmarks/Graph/spfa.cpp
@@ -2,7 +2,7 @@
 #include "../../src/Graph/spfa.cpp"
 
 double test_spfa() {
-  auto g = random_cgraph(-100, 1000);
+  auto g = random_cgraph(0, 1000);
   Timer timer;
   spfa(g, 0, (int)1e9);
   return timer.stop() / edge_num;
