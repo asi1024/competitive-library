eccae1af3cd3ab7521e28d8f078b59d22a1788b4
asi1024 <imanishi@fos.kuis.kyoto-u.ac.jp>
2016-09-04 22:33:37 +0900

    modify spfa

--- src/Graph/spfa.cpp
+++ src/Graph/spfa.cpp
@@ -2,11 +2,10 @@
 
 #include "Graph.hpp"
 
-template <typename Weight>
-pair<bool,Array<Weight>> spfa(const Graph<Weight> &g, int s) {
+template <typename Cost>
+pair<bool,vector<Cost>> spfa(const CGraph<Cost> &g, int s, Cost inf, Cost zero = 0) {
   const int n = g.size();
-  const Weight INF = 1e9;
-  Array<Weight> d(n, INF); d[s] = 0;
+  vector<Cost> d(n, inf); d[s] = zero;
   vector<int> updated(n, 0);
   vector<bool> inque(n, false);
   queue<int> que;
@@ -18,7 +17,7 @@ pair<bool,Array<Weight>> spfa(const Graph<Weight> &g, int s) {
     ++updated[from];
     if (updated[from] == n+1) return {false, d}; // negative cycle
     for (auto e: g[from]) {
-      Weight cost = d[from] + e.cost;
+      Cost cost = d[from] + e.cost;
       if (cost < d[e.to]) {
         d[e.to] = cost;
         if (!inque[e.to]) {
@@ -36,13 +35,13 @@ pair<bool,Array<Weight>> spfa(const Graph<Weight> &g, int s) {
 int main() {
   int V, E, r, s, t, d;
   cin >> V >> E >> r;
-  Graph<int> g(V);
+  CGraph<int> g(V);
   while (E--) {
     cin >> s >> t >> d;
     add_edge(g, s, t, d);
   }
   const int INF = 1e9;
-  Array<int> res = spfa(g, r).second;
+  auto res = spfa(g, r, INF).second;
   for (int i = 0; i < V; ++i) {
     if (res[i] == INF) cout << "INF" << endl;
     else cout << res[i] << endl;
@@ -56,15 +55,15 @@ int main() {
 int main() {
   int V, E, r, s, t, d;
   cin >> V >> E >> r;
-  Graph<int> g(V);
+  CGraph<int> g(V);
   while (E--) {
     cin >> s >> t >> d;
     add_edge(g, s, t, d);
   }
   const int INF = 1e9;
   bool flag;
-  Array<int> res;
-  tie(flag, res) = spfa(g, r);
+  vector<int> res;
+  tie(flag, res) = spfa(g, r, INF);
   if (flag) {
     for (int i = 0; i < V; ++i) {
       if (res[i] == INF) cout << "INF" << endl;
