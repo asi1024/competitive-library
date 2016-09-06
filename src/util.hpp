f188346e3241d0356c309d7017466d23197d416d
asi1024 <imanishi@fos.kuis.kyoto-u.ac.jp>
2016-09-07 02:42:20 +0900

    add template type infinity

--- src/util.hpp
+++ src/util.hpp
@@ -1,3 +1,11 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+template<typename T> T inf() { assert(false); }
+template<> constexpr int inf<int>() { return 1e9; }
+template<> constexpr ll inf<ll>() { return 1e18; }
+template<> constexpr ld inf<ld>() { return 1e30; }
