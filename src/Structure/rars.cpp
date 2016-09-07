90acd2ffff0a535413eed91a51cfc0433a453000
asi1024 <imanishi@fos.kuis.kyoto-u.ac.jp>
2016-09-08 01:33:45 +0900

    cosmetics

--- src/Structure/rars.cpp
+++ src/Structure/rars.cpp
@@ -8,14 +8,12 @@ class RARS {
 public:
   RARS(int n) : bit1(n+1), bit2(n+1) {}
   void add(int l, int r, T val) {
-    bit1.add(l, -val * l);
-    bit1.add(r,  val * r);
-    bit2.add(l,  val);
-    bit2.add(r, -val);
+    bit1.add(l, -val * l); bit2.add(l,  val);
+    bit1.add(r,  val * r); bit2.add(r, -val);
   }
   T sum(int l, int r) {
-    T s1 = bit1.sum(r) + bit2.sum(r) * r;
-    T s2 = bit1.sum(l) + bit2.sum(l) * l;
-    return s1 - s2;
+    T rsum = bit1.sum(r) + bit2.sum(r) * r;
+    T lsum = bit1.sum(l) + bit2.sum(l) * l;
+    return rsum - lsum;
   }
 };
