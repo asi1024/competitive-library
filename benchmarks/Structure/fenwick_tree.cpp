bb45792ab7bb53a3b80d8ce63c5e4e1fbd7954ef
asi1024 <imanishi@fos.kuis.kyoto-u.ac.jp>
2016-09-13 22:21:36 +0900

    fix fenwick tree

--- benchmarks/Structure/fenwick_tree.cpp
+++ benchmarks/Structure/fenwick_tree.cpp
@@ -4,7 +4,7 @@ vector<int> test_fenwick_tree(vector<Query> query) {
   FenwickTree<int> bit(array_len);
   vector<int> res;
   for (auto q: query) {
-    if (q.type) bit.add(q.pos, q.value);
+    if (q.type) bit.add(q.pos, q.value_small);
     else res.push_back(bit.sum(q.pos));
   }
   return res;
