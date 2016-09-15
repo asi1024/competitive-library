2a6bf11614a58cd6b2eac0ae85a00d955710d9bd
imanishi <imanishi@fos.kuis.kyoto-u.ac.jp>
2016-09-15 13:54:07 +0900

    add isis_ll test

--- benchmarks/Geometry/intersect.cpp
+++ benchmarks/Geometry/intersect.cpp
@@ -1,81 +1,79 @@
-#include "../util.hpp"
+#include "../gen_geometry.hpp"
 
-double test_isis_ll() {
-  auto l1 = random_lines();
-  auto l2 = random_lines();
-  int sum = 0;
-  Timer timer;
-  for (int i = 0; i < query_num; ++i)
-    sum += isis_ll(l1[i], l2[i]);
-  return timer.stop() / query_num + zero(sum);
+vector<int> test_isis_ll(vector<pair<Line,Line>> query) {
+  vector<int> res;
+  for (auto i: query) {
+    res.push_back(isis_ll(i.first, i.second));
+  }
+  return res;
 }
 
-double test_isis_ls() {
-  auto l1 = random_lines();
-  auto s2 = random_segments();
-  int sum = 0;
-  Timer timer;
-  for (int i = 0; i < query_num; ++i)
-    sum += isis_ls(l1[i], s2[i]);
-  return timer.stop() / query_num + zero(sum);
-}
+// double test_isis_ls() {
+//   auto l1 = random_lines();
+//   auto s2 = random_segments();
+//   int sum = 0;
+//   Timer timer;
+//   for (int i = 0; i < query_num; ++i)
+//     sum += isis_ls(l1[i], s2[i]);
+//   return timer.stop() / query_num + zero(sum);
+// }
 
-double test_isis_lp() {
-  auto l1 = random_lines();
-  auto p2 = random_points();
-  int sum = 0;
-  Timer timer;
-  for (int i = 0; i < query_num; ++i)
-    sum += isis_lp(l1[i], p2[i]);
-  return timer.stop() / query_num + zero(sum);
-}
+// double test_isis_lp() {
+//   auto l1 = random_lines();
+//   auto p2 = random_points();
+//   int sum = 0;
+//   Timer timer;
+//   for (int i = 0; i < query_num; ++i)
+//     sum += isis_lp(l1[i], p2[i]);
+//   return timer.stop() / query_num + zero(sum);
+// }
 
-double test_isis_ss() {
-  auto s1 = random_segments();
-  auto s2 = random_segments();
-  int sum = 0;
-  Timer timer;
-  for (int i = 0; i < query_num; ++i)
-    sum += isis_ss(s1[i], s2[i]);
-  return timer.stop() / query_num + zero(sum);
-}
+// double test_isis_ss() {
+//   auto s1 = random_segments();
+//   auto s2 = random_segments();
+//   int sum = 0;
+//   Timer timer;
+//   for (int i = 0; i < query_num; ++i)
+//     sum += isis_ss(s1[i], s2[i]);
+//   return timer.stop() / query_num + zero(sum);
+// }
 
-double test_isis_sp() {
-  auto s1 = random_segments();
-  auto p2 = random_points();
-  int sum = 0;
-  Timer timer;
-  for (int i = 0; i < query_num; ++i)
-    sum += isis_sp(s1[i], p2[i]);
-  return timer.stop() / query_num + zero(sum);
-}
+// double test_isis_sp() {
+//   auto s1 = random_segments();
+//   auto p2 = random_points();
+//   int sum = 0;
+//   Timer timer;
+//   for (int i = 0; i < query_num; ++i)
+//     sum += isis_sp(s1[i], p2[i]);
+//   return timer.stop() / query_num + zero(sum);
+// }
 
-double test_proj() {
-  auto l1 = random_lines();
-  auto p2 = random_points();
-  Point sum;
-  Timer timer;
-  for (int i = 0; i < query_num; ++i)
-    sum += proj(l1[i], p2[i]);
-  return timer.stop() / query_num + zero(sum);
-}
+// double test_proj() {
+//   auto l1 = random_lines();
+//   auto p2 = random_points();
+//   Point sum;
+//   Timer timer;
+//   for (int i = 0; i < query_num; ++i)
+//     sum += proj(l1[i], p2[i]);
+//   return timer.stop() / query_num + zero(sum);
+// }
 
-double test_mirror() {
-  auto l1 = random_lines();
-  auto p2 = random_points();
-  Point sum;
-  Timer timer;
-  for (int i = 0; i < query_num; ++i)
-    sum += mirror(l1[i], p2[i]);
-  return timer.stop() / query_num + zero(sum);
-}
+// double test_mirror() {
+//   auto l1 = random_lines();
+//   auto p2 = random_points();
+//   Point sum;
+//   Timer timer;
+//   for (int i = 0; i < query_num; ++i)
+//     sum += mirror(l1[i], p2[i]);
+//   return timer.stop() / query_num + zero(sum);
+// }
 
-double test_is_ll() {
-  auto l1 = random_lines();
-  auto l2 = random_lines();
-  Point sum;
-  Timer timer;
-  for (int i = 0; i < query_num; ++i)
-    sum += is_ll(l1[i], l2[i]);
-  return timer.stop() / query_num + zero(sum);
-}
+// double test_is_ll() {
+//   auto l1 = random_lines();
+//   auto l2 = random_lines();
+//   Point sum;
+//   Timer timer;
+//   for (int i = 0; i < query_num; ++i)
+//     sum += is_ll(l1[i], l2[i]);
+//   return timer.stop() / query_num + zero(sum);
+// }
