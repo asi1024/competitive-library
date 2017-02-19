e2c5b32ce84bfdfdfc835b1cc42d93d9e73c2690
Akifumi Imanishi <imanishi@fos.kuis.kyoto-u.ac.jp>
2017-02-20 04:18:42 +0900

    Enhance segment_tree

--- benchmarks/Structure/segment_tree.cpp
+++ benchmarks/Structure/segment_tree.cpp
@@ -1,7 +1,17 @@
 #include "../util.hpp"
 
+struct Data {
+  int n;
+  Data() : n(INT_MAX) {}
+  Data(int n) : n(n) {}
+};
+
+Data merge(const Data &a, const Data &b) {
+  return Data(min(a.n, b.n));
+}
+
 double test_segment_tree() {
-  SegmentTree<int, min> seg(array_len, 0x7FFFFFFF);
+  SegmentTree<Data> seg(vector<Data>(array_len, Data()));
   vector<Query> query = random_query();
   Timer timer;
   for (auto q: query) {
--- src/Structure/segment_tree.cpp
+++ src/Structure/segment_tree.cpp
@@ -2,47 +2,58 @@
 
 #include "../util.hpp"
 
-template <typename T, const T& merge(const T&, const T&)>
+template <class T>
 class SegmentTree {
-  const int n;
-  const T id;
-  vector<T> data;
-  T sub(int l, int r, int node, int lb, int ub) const {
-    if (ub <= l || r <= lb) return id;
-    if (l <= lb && ub <= r) return data[node];
-    T vl = sub(l, r, node * 2 + 0, lb, (lb + ub) / 2);
-    T vr = sub(l, r, node * 2 + 1, (lb + ub) / 2, ub);
-    return merge(vl, vr);
-  }
-  int size(int n) const {
-    return n == 1 ? n : size((n + 1) / 2) * 2;
-  }
+  const int sz, n;
+  std::vector<T> data;
+  int expand(int n) const { return n == 1 ? n : expand((n + 1) / 2) * 2; }
 public:
-  SegmentTree(int m, T id) : n(size(m)), id(id), data(n * 2, id) {}
-  void update(int p, T val) {
-    assert (0 <= p && p < n);
-    p += n;
-    data[p] = val;
-    while (p /= 2) {
-      int l = p * 2, r = p * 2 + 1;
-      data[p] = merge(data[l], data[r]);
+  SegmentTree(const std::vector<T> &init) :
+    sz(init.size()), n(expand(sz)), data(n * 2, T()) {
+    std::copy(begin(init), end(init), begin(data));
+    for (int i = n - 1; i >= 0; --i) {
+      data[i] = merge(data[i * 2 + 0], data[i * 2 + 1]);
     }
   }
+  int size() const { return sz; }
+  void update(int p, T val) {
+    assert (0 <= p && p < sz); // assertion
+    data[p += n] = val;
+    while (p /= 2) data[p] = merge(data[p * 2], data[p * 2 + 1]);
+  }
   T find(int l, int r) const {
-    return sub(l, r, 1, 0, n);
+    assert (0 <= l && l <= r && r <= sz); // assertion
+    l += n; r += n;
+    T res1 = T(), res2 = T();
+    while (l != r) {
+      if (l % 2) res1 = merge(res1, data[l++]);
+      if (r % 2) res2 = merge(data[--r], res2);
+      l /= 2; r /= 2;
+    }
+    return merge(res1, res2);
   }
 };
 
-// Verified : AOJ DSL_2_A (Range Minimum Query)
+// Verified : AOJ DSL_2_A (Range Minimum Query) 0.02sec
 /*
+struct Data {
+  int n;
+  Data() : n(INT_MAX) {}
+  Data(int n) : n(n) {}
+};
+
+Data merge(const Data &a, const Data &b) {
+  return Data(min(a.n, b.n));
+}
+
 int main() {
   int n, q, com, x, y;
-  cin >> n >> q;
-  SegmentTree<int,min> seg(n, 0x7FFFFFFF);
+  scanf("%d%d", &n, &q);
+  SegmentTree<Data> seg(vector<Data>(n, Data()));
   while (q--) {
-    cin >> com >> x >> y;
-    if (com) cout << seg.find(x, y+1) << endl;
-    else seg.update(x, y);
+    scanf("%d%d%d", &com, &x, &y);
+    if (com) printf("%d\n", seg.find(x, y+1).n);
+    else seg.update(x, Data(y));
   }
   return 0;
 }
