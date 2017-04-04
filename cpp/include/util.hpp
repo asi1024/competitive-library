0f5e8c558475d0784f6b18d446fa8afb2df587e8
Akifumi Imanishi <imanishi@fos.kuis.kyoto-u.ac.jp>
2017-04-05 04:30:10 +0900

    add util.hpp

---
+++ cpp/include/util.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+template<typename T> T inf;
+template<> constexpr int inf<int> = 1e9;
+template<> constexpr ll inf<ll> = 1e18;
+template<> constexpr ld inf<ld> = 1e30;
