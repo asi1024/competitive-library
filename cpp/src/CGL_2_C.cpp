889229346f47543566c713f236edeef1a4f7a90c
Akifumi Imanishi <imanishi@fos.kuis.kyoto-u.ac.jp>
2017-04-06 04:49:41 +0900

    add CGL_2_C

---
+++ cpp/src/CGL_2_C.cpp
@@ -0,0 +1,19 @@
+#include "../include/intersect.cpp"
+
+Point input() {
+  ld x, y;
+  scanf("%Lf%Lf", &x, &y);
+  return Point(x, y);
+}
+
+int main() {
+  int q;
+  scanf("%d", &q);
+  while (q--) {
+    Line l1(input(), input());
+    Line l2(input(), input());
+    Point res = is_ll(l1, l2);
+    printf("%.10Lf %.10Lf\n", real(res), imag(res));
+  }
+  return 0;
+}
