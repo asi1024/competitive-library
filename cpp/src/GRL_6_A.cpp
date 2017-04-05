621d8459bf544e28cd83ac30ee5008d72c5f7182
Akifumi Imanishi <imanishi@fos.kuis.kyoto-u.ac.jp>
2017-04-05 13:55:15 +0900

    add max_flow

---
+++ cpp/src/GRL_6_A.cpp
@@ -0,0 +1,13 @@
+#include "../include/max_flow.cpp"
+
+int main() {
+  int V, E, u, v, c;
+  scanf("%d%d", &V, &E);
+  Graph<int> g(V);
+  while (E--) {
+    scanf("%d%d%d", &u, &v, &c);
+    add_edge(g, u, v, c);
+  }
+  printf("%d\n", max_flow(g, 0, V-1));
+  return 0;
+}
