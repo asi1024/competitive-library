86a1c9df0f97bd98fcc2b4e65e00958bbcd235c8
Akifumi Imanishi <imanishi@fos.kuis.kyoto-u.ac.jp>
2017-04-05 13:29:06 +0900

    ad spfa

---
+++ cpp/src/GRL_1_B.cpp
@@ -0,0 +1,23 @@
+#include "../include/spfa.cpp"
+
+int main() {
+  int V, E, r, s, t, d;
+  scanf("%d%d%d", &V, &E, &r);
+  Graph<int> g(V);
+  while (E--) {
+    scanf("%d%d%d", &s, &t, &d);
+  }
+  bool flag;
+  vector<int> res;
+  tie(flag, res) = spfa(g, r);
+  if (flag) {
+    for (int i = 0; i < V; ++i) {
+      if (res[i] == inf<int>) puts("INF");
+      else printf("%d\n", res[i]);
+    }
+  }
+  else {
+    puts("NEGATIVE CYCLE");
+  }
+  return 0;
+}
