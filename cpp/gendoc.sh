1c6bcdea0ea4d837eb8cafe2494a9359be214618
Akifumi Imanishi <imanishi@fos.kuis.kyoto-u.ac.jp>
2017-04-06 21:52:36 +0900

    update docs

--- cpp/gendoc.sh
+++ cpp/gendoc.sh
@@ -5,26 +5,31 @@ set -eu
 cd `dirname $0`
 
 echo ""
-echo "## C++"
+echo "# C++"
 echo ""
-echo "| Algorithm | Validated | AOJ Problems |"
-echo "|:---------:|:---------:|:------------:|"
 
 DIR="https://github.com/asi1024/competitive-library/blob/master/cpp"
 
-for ALGORITHM in `ls include`
+for CATEGORY in `ls include`
 do
-    LIST=`grep $ALGORITHM src/* | cut -d ':' -f 1 || true`
-    ALGORITHM="[$ALGORITHM]($DIR/include/$ALGORITHM)"
-    VALIDATED='<font color="Red">No</font>'
-    AOJLIST=""
-    for j in $LIST
-    do
-        VALIDATED='<font color="ForestGreen">Yes</font>'
-        AOJLIST="$AOJLIST[`basename $j`]($DIR/$j)<br>"
-    done
-    AOJLIST=`echo $AOJLIST | sed -e 's/<br>$//g'`
-    echo "| $ALGORITHM | $VALIDATED | $AOJLIST |"
+    if [ -d "include/$CATEGORY" ]; then
+       echo "## $CATEGORY"
+       echo "| Algorithm | Validated | AOJ Problems |"
+       echo "|:---------:|:---------:|:------------:|"
+       for ALGORITHM in `ls include/$CATEGORY`
+       do
+           LIST=`grep $ALGORITHM src/* | cut -d ':' -f 1 || true`
+           ALGORITHM="[$ALGORITHM]($DIR/include/$CATEGORY/$ALGORITHM)"
+           VALIDATED='<font color="Red">No</font>'
+           AOJLIST=""
+           for j in $LIST
+           do
+               VALIDATED='<font color="ForestGreen">Yes</font>'
+               AOJLIST="$AOJLIST[`basename $j`]($DIR/$j)<br>"
+           done
+           AOJLIST=`echo $AOJLIST | sed -e 's/<br>$//g'`
+           echo "| $ALGORITHM | $VALIDATED | $AOJLIST |"
+       done
+       echo ""
+    fi
 done
-
-echo ""
