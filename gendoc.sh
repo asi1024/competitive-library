ff95b5e7126021573062cfd1fddc4b4c7ae868f4
Akifumi Imanishi <imanishi@fos.kuis.kyoto-u.ac.jp>
2017-04-12 03:47:22 +0900

    add cpp2doc

--- gendoc.sh
+++ gendoc.sh
@@ -8,4 +8,5 @@ INDEX="docs/index.md"
 
 cat docs/header.md          >  $INDEX
 ./scripts/gendoc.sh C++ cpp >> $INDEX
+./scripts/cpp2doc.sh
 cat docs/footer.md          >> $INDEX
---
+++ scripts/cpp2doc.sh
@@ -0,0 +1,13 @@
+for i in `ls cpp/include/*/* cpp/src/*`
+do
+    DOC="docs/`dirname $i`/`basename $i .cpp`.md"
+    mkdir -p "docs/`dirname $i`"
+
+    echo "## `basename $i`" >  $DOC
+    echo ""                 >> $DOC
+    echo "\`\`\`"           >> $DOC
+    cat $i | sed '/^#pragma/d' | sed '/^#include/d' | sed '/./,$!d' >> $DOC
+    echo "\`\`\`"           >> $DOC
+    echo ""                 >> $DOC
+    echo "- [Back](../)"    >> $DOC
+done
