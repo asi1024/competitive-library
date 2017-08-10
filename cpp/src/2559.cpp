a2c02f4857e44eb4f463c41a4a753c6aa51858de
Akifumi Imanishi <imanishi@fos.kuis.kyoto-u.ac.jp>
2017-08-10 20:43:46 +0900

    Update 2559

--- cpp/src/2559.cpp
+++ cpp/src/2559.cpp
@@ -1,61 +1,8 @@
-#include <bits/stdc++.h>
+#include "../include/structure/unionfind.cpp"
+#include "../include/structure/skew_heap.cpp"
 
 // #define BEGIN_STACK_EXTEND(size) void * stack_extend_memory_ = malloc(size);void * stack_extend_origin_memory_;char * stack_extend_dummy_memory_ = (char*)alloca((1+(int)(((long long)stack_extend_memory_)&127))*16);*stack_extend_dummy_memory_ = 0;asm volatile("mov %%rsp, %%rbx\nmov %%rax, %%rsp":"=b"(stack_extend_origin_memory_):"a"((char*)stack_extend_memory_+(size)-1024));
 // #define END_STACK_EXTEND asm volatile("mov %%rax, %%rsp"::"a"(stack_extend_origin_memory_));free(stack_extend_memory_);
-
-using namespace std;
-
-using ll = long long;
-
-class UnionFind {
-  vector<int> p;
-public:
-  UnionFind (int n) : p(n, -1) {}
-  int root(int x) {
-    return p[x] < 0 ? x : p[x] = root(p[x]);
-  }
-  bool same(int x, int y) {
-    return root(x) == root(y);
-  }
-  bool unite(int x, int y) {
-    x = root(x); y = root(y);
-    if (x == y) return false;
-    if (p[y] < p[x]) swap(x, y);
-    if (p[x] == p[y]) --p[x];
-    p[y] = x;
-    return true;
-  }
-};
-
-template<typename T>
-struct skew_heap {
-  T val;
-  skew_heap *l, *r;
-  skew_heap(T v) : val(v), l(nullptr), r(nullptr) {}
-};
-
-template<typename T>
-skew_heap<T> *meld(skew_heap<T> *a, skew_heap<T> *b) {
-  if (!a) return b;
-  if (!b) return a;
-  if (a->val > b->val) swap(a, b);
-  a->r = meld(a->r, b);
-  swap(a->l, a->r);
-  return a;
-}
-
-template<typename T>
-skew_heap<T> *push(skew_heap<T> *h, T v) {
-  skew_heap<T> *p = new skew_heap<T>(v);
-  return meld(h, p);
-}
-
-template<typename T>
-skew_heap<T> *pop(skew_heap<T> *h) {
-  skew_heap<T> *res = meld(h->l, h->r);
-  free(h);
-  return res;
-}
 
 using Heap = skew_heap<tuple<int,int,int>>*;
 
