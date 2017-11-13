72af7e50e68c50cd3cac3678c72874eb178f306f
Akifumi Imanishi <imanishi@fos.kuis.kyoto-u.ac.jp>
2017-11-13 14:04:15 +0900

    Support multiple tests

--- cpp/spec.sh
+++ cpp/spec.sh
@@ -25,12 +25,13 @@ done
 
 for i in `ls src/*.cpp`
 do
-    PROBLEM_ID=`basename $i .cpp`
+    FILENAME=`basename $i .cpp`
+    PROBLEM_ID=`echo $FILENAME | tr '.' '\n' | head -n 1`
     FLAG=0
 
     for j in $@
     do
-        if [ $j == $PROBLEM_ID ] || [ $j == "--all" ] || [ $j == "-all" ]; then
+        if [ $j == $FILENAME ] || [ $j == "--all" ] || [ $j == "-all" ]; then
             FLAG=1
         fi
     done
@@ -39,9 +40,9 @@ do
     fi
 
     # compile
-    echo -e "Compiling $PROBLEM_ID.cpp ...\r\c"
+    echo -e "Compiling ${FILENAME}.cpp ...\r\c"
     compile $i
-    echo -e "Compiling $PROBLEM_ID.cpp done."
+    echo -e "Compiling ${FILENAME}.cpp done."
     if [ $BUILD_ONLY == 1 ]; then
         continue
     fi
@@ -54,7 +55,7 @@ do
     for j in `ls -v $TESTSUITES/$PROBLEM_ID/*.in`
     do
         TESTCASE=`basename $j .in`
-        echo -e "$PROBLEM_ID: $TESTCASE.in\r\c"
+        echo -e "$FILENAME: $TESTCASE.in\r\c"
         TIMEFORMAT=%R
         LOG=`(time timeout -s 9 $TIMELIMIT ./exec < $j > out 2> log) 2>&1` || (
             LOG=`echo $LOG | tail -n 1`
@@ -62,12 +63,12 @@ do
             echo ""
             if [ `echo "$TIME > 1" | bc -l` == 1 ]; then
                 echo "time: $TIME sec"
-                echo -e "$PROBLEM_ID: \033[0;33mtime limit exceeded\033[0m in #$TESTCASE."
+                echo -e "$FILENAME: \033[0;33mtime limit exceeded\033[0m in #$TESTCASE."
             else
                 echo "===== error ====="
                 cat log
                 echo "time: $TIME sec"
-                echo -e "$PROBLEM_ID: \033[0;35mruntime error\033[0m in #$TESTCASE."
+                echo -e "$FILENAME: \033[0;35mruntime error\033[0m in #$TESTCASE."
             fi
             rm -f exec out log
             exit 1)
@@ -79,11 +80,11 @@ do
         python $TESTSUITES/diff.py out "`dirname $j`/$TESTCASE.out" || (
             echo ""
             echo "time: $TIME sec"
-            echo -e "$PROBLEM_ID: \033[0;31mwrong answer\033[0m in #$TESTCASE."
+            echo -e "$FILENAME: \033[0;31mwrong answer\033[0m in #$TESTCASE."
             rm -f exec out log
             exit 1)
     done
-    echo -e "$PROBLEM_ID: \033[0;32mpassed\033[0m (time: $MAX_TIME sec)."
+    echo -e "$FILENAME: \033[0;32mpassed\033[0m (time: $MAX_TIME sec)."
 done
 
 rm -f exec out log
