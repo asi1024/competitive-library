efb6f6ae6b8e234000735aeb4601446353327b95
Akifumi Imanishi <imanishi@fos.kuis.kyoto-u.ac.jp>
2018-01-04 05:22:59 +0900

    Add OCaml CI settings

--- gendoc.sh
+++ gendoc.sh
@@ -4,7 +4,7 @@ set -eu
 
 cd `dirname $0`
 
-python cpp/docs/title.py        > docs/cpp/index.md
 python cpp/docs/documents.py
-python ocaml/docs/title.py      > docs/ocaml/index.md
+python cpp/docs/title.py        > docs/cpp/index.md
 python ocaml/docs/documents.py
+python ocaml/docs/title.py      > docs/ocaml/index.md
