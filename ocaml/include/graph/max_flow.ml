a52069a1cab8bdac2b3121fc1e9ade293a5273ad
Akifumi Imanishi <imanishi@fos.kuis.kyoto-u.ac.jp>
2018-01-05 22:48:30 +0900

    [ocaml] Use typeinfo in dinic

--- ocaml/include/graph/max_flow.ml
+++ ocaml/include/graph/max_flow.ml
@@ -1,13 +1,15 @@
+(*+ import include/template/typeinfo.ml +*)
+
 type 't edge = { s : int; t : int; cap : 't ref; rev : 't ref }
 type 't graph = 't edge list array
 
-let add_edge g s t cap zero =
+let add_edge ty g s t cap =
   let cap = ref cap in
-  let rev = ref zero in
+  let rev = ref ty.zero in
   g.(s) <- { s = s; t = t; cap = cap; rev = rev } :: g.(s);
   g.(t) <- { s = t; t = s; cap = rev; rev = cap } :: g.(t)
 
-let max_flow g s t zero inf plus minus =
+let max_flow ty g s t =
   let v = Array.length g in
   let rec loop res =
     let d = Array.make v (-1) in
@@ -17,7 +19,7 @@ let max_flow g s t zero inf plus minus =
     while not (Queue.is_empty que) do
       let v = Queue.pop que in
       let update e =
-        if !(e.cap) > zero && d.(e.t) < 0
+        if !(e.cap) > ty.zero && d.(e.t) < 0
         then (d.(e.t) <- d.(v) + 1; Queue.push e.t que) in
       List.iter update g.(v)
     done;
@@ -29,20 +31,20 @@ let max_flow g s t zero inf plus minus =
            then f
            else let rec iter_loop () =
                   ( match iter.(v) with
-                    | [] -> zero
+                    | [] -> ty.zero
                     | e :: new_iter ->
                        iter.(v) <- new_iter;
-                       if !(e.cap) > zero && d.(v) < d.(e.t)
+                       if !(e.cap) > ty.zero && d.(v) < d.(e.t)
                        then let ff = augument e.t (min f !(e.cap)) in
-                            if ff > zero
-                            then ( e.cap := minus !(e.cap) ff;
-                                   e.rev := plus !(e.rev) ff;
+                            if ff > ty.zero
+                            then ( e.cap := ty.sub !(e.cap) ff;
+                                   e.rev := ty.add !(e.rev) ff;
                                    ff )
                             else iter_loop ()
                        else iter_loop () ) in
                 iter_loop () in
          let rec loop_augument res =
-           let f = augument s inf in
-           if f > zero then loop_augument (plus res f) else res in
+           let f = augument s ty.inf in
+           if f > ty.zero then loop_augument (ty.add res f) else res in
          loop (loop_augument res) in
-  loop zero
+  loop ty.zero
