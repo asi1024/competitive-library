16036132bc26f828aab0009d38902331281169f3
Akifumi Imanishi <imanishi@fos.kuis.kyoto-u.ac.jp>
2018-02-13 14:57:04 +0900

    Avoid variable shadowing

--- cpp/include/math/fmt.cpp
+++ cpp/include/math/fmt.cpp
@@ -5,10 +5,10 @@
 
 template<int prim_root, int mod, int sign>
 void FMT(vector<Modulo<mod, true>> &a) {
-  using Mod = Modulo<mod, true>;
+  using mod_t = Modulo<mod, true>;
   const int n = a.size();
-  Mod h = Mod(prim_root) ^ ((mod - 1) / n);
-  if (sign == -1) h = Mod(1) / h;
+  mod_t h = mod_t(prim_root) ^ ((mod - 1) / n);
+  if (sign == -1) h = mod_t(1) / h;
   int x = 0;
   for (int i = 1; i < n - 1; ++i) {
     for (int j = n / 2; j > (x ^= j); j /= 2);
@@ -16,11 +16,11 @@ void FMT(vector<Modulo<mod, true>> &a) {
   }
   for (int m = 1; m < n; m *= 2) {
     const int m2 = 2 * m;
-    const Mod base = Mod(h) ^ (n / m2);
-    Mod w = 1;
+    const mod_t base = mod_t(h) ^ (n / m2);
+    mod_t w = 1;
     for (int i = 0; i < m; ++i) {
       for (int j = i; j < n; j += m2) {
-        const Mod u = a[j], d = a[j + m] * w;
+        const mod_t u = a[j], d = a[j + m] * w;
         a[j] = u + d;
         a[j + m] = u - d;
       }
@@ -32,14 +32,14 @@ void FMT(vector<Modulo<mod, true>> &a) {
 template<int prim_root, int mod>
 vector<Modulo<mod, true>> convolution(vector<Modulo<mod, true>> a,
                                       vector<Modulo<mod, true>> b) {
-  using Mod = Modulo<mod, true>;
+  using mod_t = Modulo<mod, true>;
   int size = a.size() + b.size();
   while ((size & -size) != size) size += (size & -size);
   a.resize(size); FMT<prim_root, mod, 1>(a);
   b.resize(size); FMT<prim_root, mod, 1>(b);
   for (int i = 0; i < size; ++i) a[i] *= b[i];
   FMT<prim_root, mod, -1>(a);
-  const Mod inv = Mod(1) / Mod(size);
+  const mod_t inv = mod_t(1) / mod_t(size);
   for (auto &x : a) x *= inv;
   return a;
 }
