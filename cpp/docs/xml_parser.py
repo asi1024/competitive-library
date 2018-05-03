18aa42b2a8a1bab6bb7a6e5e5b13240e8a386b9a
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2018-05-03 19:51:29 +0900

    cosmetics

--- cpp/docs/xml_parser.py
+++ cpp/docs/xml_parser.py
@@ -22,51 +22,53 @@ def tostring(node):
 def filter_kind(nodes, tag):
     return [node for node in nodes if node.attrib['kind'] == tag][0]
 
-def print_function(node):
-    print('## {}\n'.format(node.find('name').text))
+def function(node):
+    res = ''
+    res += '## {}\n\n'.format(node.find('name').text)
 
-    print('```')
-    print(node.find('definition').text + node.find('argsstring').text)
-    print('```\n')
+    res += '```\n{}{}\n```\n\n'.format(
+        node.find('definition').text, node.find('argsstring').text)
 
     if node.find('briefdescription/para') is not None:
-        print(node.find('briefdescription/para').text + '\n')
+        res += node.find('briefdescription/para').text + '\n\n'
 
     description_node = node.find('inbodydescription/para')
 
     if description_node is None:
-        return
+        return res.strip()
 
     params_list = description_node.findall('parameterlist/parameteritem')
     if len(params_list) > 0:
-        print('|:--:|:--|')
+        res += '|:--:|:--|\n'
         for elem in params_list:
             params = ', '.join(_.find('parametername').text
                                for _ in elem.findall('parameternamelist'))
             description = elem.find('parameterdescription/para').text.strip()
-            print('|{}|{}|'.format(params, description))
-        print()
+            res += '|{}|{}|\n'.format(params, description)
+        res += '\n'
 
     ## Type requirements (TODO)
     
     simplesect = description_node.findall('simplesect')
 
-    print('### Return value\n')
-    print(filter_kind(simplesect, 'return').find('para').text + '\n')
+    res += '### Return value\n\n'
+    res += filter_kind(simplesect, 'return').find('para').text + '\n\n'
 
-    print('### Precondition')
-    print(filter_kind(simplesect, 'pre').find('para').text + '\n')
+    res += '### Precondition\n\n'
+    res += filter_kind(simplesect, 'pre').find('para').text + '\n\n'
 
-    print('### Time Complexity')
-    print(filter_kind(simplesect, 'post').find('para').text + '\n')
+    res += '### Time Complexity\n\n'
+    res += filter_kind(simplesect, 'post').find('para').text + '\n\n'
+
+    return res.strip()
 
 
 def main(filepath):
     root = ET.fromstring(''.join(_ for _ in open(filepath)))
 
     for elem in get_function(root):
-        print_function(elem)
-    
+        print(function(elem))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
