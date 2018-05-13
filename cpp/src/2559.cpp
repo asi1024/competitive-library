136f0fd89c2811e14d182e42385734e79bf15244
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2018-05-14 03:13:06 +0900

    Fix

--- cpp/src/2559.cpp
+++ cpp/src/2559.cpp
@@ -1,8 +1,11 @@
+#include "../include/template/const_value.hpp"
 #include "../include/structure/unionfind.cpp"
 #include "../include/structure/skew_heap.cpp"
 
 // #define BEGIN_STACK_EXTEND(size) void * stack_extend_memory_ = malloc(size);void * stack_extend_origin_memory_;char * stack_extend_dummy_memory_ = (char*)alloca((1+(int)(((long long)stack_extend_memory_)&127))*16);*stack_extend_dummy_memory_ = 0;asm volatile("mov %%rsp, %%rbx\nmov %%rax, %%rsp":"=b"(stack_extend_origin_memory_):"a"((char*)stack_extend_memory_+(size)-1024));
 // #define END_STACK_EXTEND asm volatile("mov %%rax, %%rsp"::"a"(stack_extend_origin_memory_));free(stack_extend_memory_);
+
+using namespace std;
 
 using Heap = skew_heap<tuple<int,int,int>>*;
 
