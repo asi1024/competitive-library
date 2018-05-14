35095841ae560c1ea76f91a1873867aca375a189
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2018-05-15 02:22:27 +0900

    Extension of cin/cout

---
+++ cpp/include/template/cin.cpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "includes.hpp"
+
+template<typename T, int M, int N>
+struct tuple_getter {
+  static void get(std::istream& is, T& x) {
+    is >> std::get<M>(x);
+    tuple_getter<T, M + 1, N>::get(is, x);
+  }
+};
+
+template<typename T, int N>
+struct tuple_getter<T, N, N> {
+  static void get(std::istream& is, T& x) {
+    is >> std::get<N>(x);
+  }
+};
+
+template<typename... Args>
+std::istream& operator>>(std::istream& is, std::tuple<Args...>& t) {
+  tuple_getter<std::tuple<Args...>, 0, sizeof...(Args) - 1>::get(is, t);
+  return is;
+}
+
+template <typename T, typename U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+  is >> p.first >> p.second;
+  return is;
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+  for (T &x: v) is >> x;
+  return is;
+}
--- cpp/include/template/cout.cpp
+++ cpp/include/template/cout.cpp
@@ -1,7 +1,36 @@
-#include "../util.hpp"
+#pragma once
 
-template <class T>
-ostream &operator<<(ostream &os, const vector<T> &v) {
+#include "includes.hpp"
+
+template<typename T, int M, int N>
+struct tuple_printer {
+  static void print(std::ostream& os, const T& x) {
+    os << std::get<M>(x) << " ";
+    tuple_printer<T, M + 1, N>::print(os, x);
+  }
+};
+
+template<typename T, int N>
+struct tuple_printer<T, N, N> {
+  static void print(std::ostream& os, const T& x) {
+    os << std::get<N>(x);
+  }
+};
+
+template<typename... Args>
+std::ostream& operator<<(std::ostream& os, const std::tuple<Args...>& t) {
+  tuple_printer<std::tuple<Args...>, 0, sizeof...(Args) - 1>::print(os, t);
+  return os;
+}
+
+template <typename T, typename U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+  os << p.first << " " << p.second;
+  return os;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
   for (auto it = v.begin(); it != v.end(); ++it) {
     if (it != v.begin()) os << " ";
     os << *it;
