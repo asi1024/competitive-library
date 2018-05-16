104d292a60b6902f718f7ec494481043cb1dd075
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2018-05-17 03:46:22 +0900

    Fix

--- cpp/include/template/const_value.hpp
+++ cpp/include/template/const_value.hpp
@@ -7,9 +7,6 @@ template<> constexpr int inf<int>() { return 1e9; }
 template<> constexpr long long inf<long long>() { return 1e18; }
 template<> constexpr long double inf<long double>() { return 1e30; }
 
-
-template<typename T> T constexpr zero();
-
 template<typename T> constexpr
 typename std::enable_if<std::is_integral<T>::value, T>::type
 zero() { return T(0); }
