c1870928a1ed94977e96a1a1c1d939b31f200a3f
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2018-05-20 18:12:19 +0900

    Add Hopcroft-Karp

--- cpp/include/graph/bipartite_matching.cpp
+++ cpp/include/graph/bipartite_matching.cpp
@@ -1,38 +1,67 @@
 #pragma once
 
-#include "../util.hpp"
+#include "../template/includes.hpp"
 
 class BipartiteMatching {
-  int size;
-  vector<vector<int>> g;
-  vector<int> match;
-  vector<bool> used;
-  bool dfs(int v) {
-    used[v] = true;
-    for (int u : g[v]) {
-      int w = match[u];
-      if (w < 0 || (!used[w] && dfs(w))) {
-        match[v] = u;
-        match[u] = v;
-        return true;
+  static const int INF = 1000000000;
+  const int n1, n;
+  std::vector<std::vector<int>> g;
+  std::vector<int> match, dist;
+  bool bfs() {
+    std::queue<int> que;
+    std::fill(std::begin(dist), std::begin(dist) + n1, INF);
+    for (int i = 0; i < n1; ++i) {
+      if (match[i] == n) {
+        dist[i] = 0;
+        que.push(i);
       }
     }
-    return false;
+    dist[n] = INF;
+    while (!que.empty()) {
+      int u = que.front();
+      que.pop();
+      if (dist[u] < dist[n]) {
+        for (int v : g[u]) {
+          if (dist[match[v]] == INF) {
+            dist[match[v]] = dist[u] + 1;
+            que.push(match[v]);
+          }
+        }
+      }
+    }
+    return (dist[n] != INF);
+  }
+  bool dfs(int u) {
+    if (u != n) {
+      for (int v : g[u]) {
+        if (dist[match[v]] == dist[u] + 1 && dfs(match[v])) {
+          match[v] = u;
+          match[u] = v;
+          return true;
+        }
+      }
+      dist[u] = INF;
+      return false;
+    }
+    return true;
   }
 
 public:
-  BipartiteMatching(int v) : size(v), g(v), match(v), used(v) {}
-  void add_edge(int u, int v) {
-    g[u].push_back(v);
-    g[v].push_back(u);
+  BipartiteMatching(int v1, int v2) :
+    n1(v1), n(v1 + v2), g(n + 1), match(n + 1), dist(n + 1) {
+    ;
   }
-  int maximum_matching(void) {
+  void add(int u, int v) {
+    g[u].push_back(v + n1);
+    g[v + n1].push_back(u);
+  }
+  int maximum_matching() {
     int res = 0;
-    fill(begin(match), end(match), -1);
-    for (int v = 0; v < size; ++v) {
-      if (match[v] >= 0) continue;
-      fill(begin(used), end(used), 0);
-      if (dfs(v)) ++res;
+    std::fill(std::begin(match), std::begin(match) + n, n);
+    while (bfs()) {
+      for (int i = 0; i < n1; ++i) {
+        if (match[i] == n && dfs(i)) ++res;
+      }
     }
     return res;
   }
--- cpp/src/GRL_7_A.cpp
+++ cpp/src/GRL_7_A.cpp
@@ -3,10 +3,10 @@
 int main() {
   int X, Y, E, x, y;
   scanf("%d%d%d", &X, &Y, &E);
-  BipartiteMatching mat(X + Y);
+  BipartiteMatching mat(X, Y);
   while (E--) {
     scanf("%d%d", &x, &y);
-    mat.add_edge(x, X + y);
+    mat.add(x, y);
   }
   printf("%d\n", mat.maximum_matching());
   return 0;
