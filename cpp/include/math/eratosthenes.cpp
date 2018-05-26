6bb0a987a145550463842a343ba0061f265f6254
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2018-05-26 16:27:33 +0900

    Fix eratosthenes

--- cpp/include/math/eratosthenes.cpp
+++ cpp/include/math/eratosthenes.cpp
@@ -1,25 +1,17 @@
 #pragma once
 
-#include "../util.hpp"
+#include "../template/includes.hpp"
 
-const int Prime_N = 1024000;
-
-bitset<Prime_N> is_prime;
-
-vector<int> primes() {
-  is_prime.set();
-  is_prime[0] = false;
-  is_prime[1] = false;
-  for (int i = 2; i * i < Prime_N; ++i) {
-    if (is_prime[i]) {
-      for (int j = i * i; j < Prime_N; j += i) {
-        is_prime[j] = false;
+std::vector<int> eratosthenes(int M) {
+  std::vector<int> res(M, 0);
+  res[0] = -1;
+  res[1] = -1;
+  for (int i = 2; i * i < M; ++i) {
+    if (res[i] == 0) {
+      for (int j = i * i; j < M; j += i) {
+        if (res[j] == 0) res[j] = i;
       }
     }
-  }
-  vector<int> res;
-  for (int i = 2; i < Prime_N; ++i) {
-    if (is_prime[i]) res.push_back(i);
   }
   return res;
 }
