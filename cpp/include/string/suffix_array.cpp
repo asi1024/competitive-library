49d2953518f5a8f9a1423c7be87b586ff371f48e
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2018-05-27 01:08:54 +0900

    Fix suffix array

--- cpp/include/string/suffix_array.cpp
+++ cpp/include/string/suffix_array.cpp
@@ -2,13 +2,12 @@
 
 #include "../structure/monoid.hpp"
 #include "../structure/segment_tree.cpp"
-#include "../util.hpp"
 
 template <typename string_t> struct SuffixArray {
   struct SAComp {
     const int h;
-    const vector<int> &g;
-    SAComp(int h_, vector<int> &g_) : h(h_), g(g_) { ; }
+    const std::vector<int> &g;
+    SAComp(int h_, std::vector<int> &g_) : h(h_), g(g_) { ; }
     bool operator()(int a, int b) {
       return a != b && (g[a] != g[b] ? g[a] < g[b] : g[a + h] < g[b + h]);
     }
@@ -16,19 +15,19 @@ template <typename string_t> struct SuffixArray {
 
   int n;
   string_t str;
-  vector<int> sa, lcp;
+  std::vector<int> sa, lcp;
 
   SuffixArray(const string_t &t) : n(t.size()), str(t), sa(n + 1), lcp(n + 1) {
     // build SA
-    vector<int> g(n + 1, 0), b(n + 1, 0);
+    std::vector<int> g(n + 1, 0), b(n + 1, 0);
     for (int i = 0; i <= n; ++i) {
       sa[i] = i;
       g[i] = str[i];
     }
-    sort(begin(sa), end(sa), SAComp(0, g));
+    std::sort(begin(sa), end(sa), SAComp(0, g));
     for (int h = 1; b[n] != n; h *= 2) {
       SAComp comp(h, g);
-      sort(sa.begin(), sa.end(), comp);
+      std::sort(sa.begin(), sa.end(), comp);
       for (int i = 0; i < n; ++i) b[i + 1] = b[i] + comp(sa[i], sa[i + 1]);
       for (int i = 0; i <= n; ++i) g[sa[i]] = b[i];
     }
@@ -61,10 +60,10 @@ template <typename string_t> struct SuffixArray {
     return ub;
   }
   int lower_bound(const string_t &t) const {
-    return binary_search<less<int>>(t);
+    return binary_search<std::less<int>>(t);
   }
   int upper_bound(const string_t &t) const {
-    return binary_search<less_equal<int>>(t);
+    return binary_search<std::less_equal<int>>(t);
   }
   int find(const string_t &t) const {
     const int m = t.size();
@@ -75,12 +74,12 @@ template <typename string_t> struct SuffixArray {
 
 class LCP {
   int n;
-  vector<int> mapsto;
+  std::vector<int> mapsto;
   SegmentTree<RMQ<int>> seg;
 
 public:
-  LCP(const string &str) : n(str.size()), mapsto(n), seg(n) {
-    SuffixArray<string> sa(str);
+  LCP(const std::string &str) : n(str.size()), mapsto(n), seg(n) {
+    SuffixArray<std::string> sa(str);
     for (int i = 0; i < n; ++i) mapsto[sa.sa[i + 1]] = i;
     for (int i = 0; i < n - 1; ++i) seg.update(i, sa.lcp[i + 2]);
   }
@@ -88,6 +87,6 @@ public:
     if (i == j) return n - i;
     i = mapsto[i];
     j = mapsto[j];
-    return seg.query(min(i, j), max(i, j));
+    return seg.query(std::min(i, j), std::max(i, j));
   }
 };
