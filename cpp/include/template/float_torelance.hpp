b44ac5483a943dd210a9704ca8fce241a22adc75
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2018-05-27 02:21:44 +0900

    Rename

--- cpp/include/template/float_torelance.hpp
+++ cpp/include/template/float_torelance.hpp
@@ -1,8 +1,11 @@
+#include "includes.hpp"
+
 template <typename float_type, const float_type &eps> class float_torelance {
   float_type x;
-
 public:
+  float_torelance() { ; }
   float_torelance(float_type value) : x(value) { ; }
+  float_type &value() { return x; }
   float_torelance operator+(const float_torelance &r) { return x + r.x; }
   float_torelance operator-(const float_torelance &r) { return x - r.x; }
   float_torelance operator*(const float_torelance &r) { return x * r.x; }
@@ -20,5 +23,17 @@ public:
   }
 };
 
+template <typename float_type, const float_type &eps>
+std::istream &operator>>(std::istream &is, float_torelance<float_type, eps> x) {
+  is >> x.value();
+  return is;
+}
+
+template <typename float_type, const float_type &eps>
+std::ostream &operator<<(std::ostream &os, float_torelance<float_type, eps> x) {
+  os << x;
+  return os;
+}
+
 constexpr long double epsilon11 = 1e-11;
-using real = float_torelance<long double, epsilon11>;
+using float11 = float_torelance<long double, epsilon11>;
