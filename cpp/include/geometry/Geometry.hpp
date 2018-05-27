2b3c9638a69e4a78b599bf1e01b56d234301cb8e
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2018-05-28 00:15:07 +0900

    Fix scc

--- cpp/include/geometry/Geometry.hpp
+++ cpp/include/geometry/Geometry.hpp
@@ -7,57 +7,65 @@
 template <typename real_t> using Vector = std::complex<real_t>;
 
 template <typename real_t> class Point {
+public:
   std::complex<real_t> p;
   Point() : p(0.0, 0.0) { ; }
-  Point(std::complex<real_t> p_) : p(p_) { ; }
+  explicit Point(std::complex<real_t> p_) : p(p_) { ; }
   Point(real_t x, real_t y) : p(x, y) { ; }
-  Vector<real_t> operator-(const Point &r){ return p - r.p } Point
-  operator+(const Vector<real_t> &r){ return p + r.p } Point
-  operator-(const Vector<real_t> &r) {
-    return p - r.p
-  }
+  Vector<real_t> operator-(const Point &r) const { return p - r.p; }
+  Point operator+(const Vector<real_t> &r) const { return p + r.p; }
+  Point operator-(const Vector<real_t> &r) const { return p - r.p; }
 };
 
 template <typename real_t>
 std::istream &operator>>(std::istream &is, Point<real_t> &p) {
-  is >> x.p.real() >> x.p.imag();
+  real_t x, y;
+  is >> x >> y;
+  p = Point<real_t>(x, y);
   return is;
 }
 
-template <typename float_type, const float_type &eps>
+template <typename real_t>
 std::ostream &operator<<(std::ostream &os, const Point<real_t> &p) {
-  os << x.p.real() << " " << x.p.imag();
+  os << p.p.real() << " " << p.p.imag();
   return os;
 }
 
-template <typename point_t> class Polygon {
-  std::vector<point_t> g;
-  Polygon() : g(0) { ; }
-  Polygon(const int n) : g(n, point_t()) { ; }
-  Polygon(const std::vector<point_t> &g_) : g(g_) { ; }
-  void push_back(const point_t &p) { g.push_back(p); }
-  point_t &front() { return g.front(); }
-  point_t &back() { return g.back(); }
-  int size() const { return g.size(); }
-  point_t &operator[](int i) {
-    i %= size();
-    return g[i < 0 ? i + size() : i];
-  }
-};
+// template <typename point_t> class Polygon {
+//   std::vector<point_t> g;
+//   Polygon() : g(0) { ; }
+//   Polygon(const int n) : g(n, point_t()) { ; }
+//   Polygon(const std::vector<point_t> &g_) : g(g_) { ; }
+//   void push_back(const point_t &p) { g.push_back(p); }
+//   point_t &front() { return g.front(); }
+//   point_t &back() { return g.back(); }
+//   int size() const { return g.size(); }
+//   point_t &operator[](int i) {
+//     i %= size();
+//     return g[i < 0 ? i + size() : i];
+//   }
+// };
 
-// ld dot(Point a, Point b) { return real(conj(a) * b); }
-// ld cross(Point a, Point b) { return imag(conj(a) * b); }
+template <typename real_t> real_t dot(Vector<real_t> a, Vector<real_t> b) {
+  return real(conj(a) * b);
+}
+template <typename real_t> real_t cross(Vector<real_t> a, Vector<real_t> b) {
+  return imag(conj(a) * b);
+}
 
-// // counter clockwise
-// int ccw(Point a, Point b, Point c) {
-//   b -= a;
-//   c -= a;
-//   if (cross(b, c) > 0) return 1;    // counter clockwise
-//   else if (cross(b, c) < 0) return -1;  // clockwise
-//   else if (dot(b, c) < 0) return 2;        // c--a--b on line
-//   else if (norm(b) < norm(c)) return -2;   // a--b--c on line
-//   else return 0;                           // a--c--b on line
-// }
+// counter clockwise
+template <typename real_t> int ccw(Vector<real_t> base, Vector<real_t> target) {
+  if (cross(base, target) > 0)
+    return 1;  // counter clockwise
+  else if (cross(base, target) < 0)
+    return -1;  // clockwise
+  else if (dot(base, target) < 0)
+    return 2;  // c--a--b on line
+  else if (norm(base) < norm(target))
+    return -2;  // a--b--c on line
+  else
+    return 0;  // a--c--b on line
+}
 
 // std::vector<Point> unique(std::vector<Point> ps) {
 //   std::sort(std::begin(ps), std::end(ps), comp);
