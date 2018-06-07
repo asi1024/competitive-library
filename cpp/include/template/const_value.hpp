0baea829bc8d2db4c19ca5de78b484798f58d472
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2018-06-07 10:51:48 +0900

    Fix const_value

--- cpp/include/template/const_value.hpp
+++ cpp/include/template/const_value.hpp
@@ -2,10 +2,17 @@
 
 #pragma once
 
-template <typename T> constexpr T inf();
-template <> constexpr int inf<int>() { return 1e9; }
-template <> constexpr long long inf<long long>() { return 1e18; }
-template <> constexpr long double inf<long double>() { return 1e30; }
+template <typename T>
+constexpr typename std::enable_if<std::is_integral<T>::value, T>::type
+inf() {
+  return std::numeric_limits::max() / 2 - 1000;
+}
+
+template <typename T>
+constexpr typename std::enable_if<std::is_floating_point<T>::value, T>::type
+inf() {
+  return 1e30;
+}
 
 template <typename T>
 constexpr typename std::enable_if<std::is_arithmetic<T>::value, T>::type
