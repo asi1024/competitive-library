05e0e5410f1add844657aeadffbf0eb09f998703
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2018-06-07 11:14:30 +0900

    Fix and docs

--- cpp/include/template/const_value.hpp
+++ cpp/include/template/const_value.hpp
@@ -2,18 +2,22 @@
 
 #pragma once
 
+/// @return Returns a large finite value representable by the numeric type T.
+/// @note It is guaranteed that the return values is strictly less than std::numeric_limits<T>::max() / 2.
 template <typename T>
-constexpr typename std::enable_if<std::is_integral<T>::value, T>::type
-inf() {
-  return std::numeric_limits::max() / 2 - 1000;
+constexpr typename std::enable_if<std::is_integral<T>::value, T>::type inf() {
+  return std::numeric_limits<T>::max() / 2 - 1000;
 }
 
+/// @return Returns a large finite value representable by the numeric type T.
+/// @note It is guaranteed that the return values is strictly less than std::numeric_limits<T>::max() / 2.
 template <typename T>
 constexpr typename std::enable_if<std::is_floating_point<T>::value, T>::type
 inf() {
-  return 1e30;
+  return std::min(std::numeric_limits<T>::max() / 2 - 1000, T(1e50));
 }
 
+/// @return Returns the zero value representable by the numeric type T.
 template <typename T>
 constexpr typename std::enable_if<std::is_arithmetic<T>::value, T>::type
 zero() {
