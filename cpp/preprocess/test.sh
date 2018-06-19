64b0d079a21bc260d9b0b367c71845566b2fe684
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2018-06-20 03:36:58 +0900

    Fix test

--- cpp/preprocess/test.sh
+++ cpp/preprocess/test.sh
@@ -7,6 +7,15 @@ echo "CXXFLAGS = $CXXFLAGS"
 
 cd `dirname $0`
 
+for file in `ls ../include/exec/*.cpp`; do
+    base=`basename ${file}`
+    echo -n "Compiling ${base} ... "
+    python preprocess.py ${file} > temp.cpp
+    ${CXX} ${CXXFLAGS} -o exec temp.cpp
+    rm temp.cpp
+    echo "ok"
+done
+
 for file in `ls ../src/*`; do
     base=`basename ${file}`
     echo -n "Compiling ${base} ... "
