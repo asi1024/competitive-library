014fc401d6b93ceddc6091cb2cb8a84541b01a4b
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2018-06-27 01:06:57 +0900

    Fix FFT

--- cpp/include/math/fft.cpp
+++ cpp/include/math/fft.cpp
@@ -3,26 +3,29 @@
 #include "../template/includes.hpp"
 #include "../template/typedef.hpp"
 
-std::vector<std::complex<ld>> FFT(const std::vector<std::complex<ld>> &a, int m) {
-  ld theta = 2.0 * pi / m;
-  const int n = a.size();
-  std::vector<std::complex<ld>> res = a;
-  for (int m = n; m >= 2; m /= 2, theta *= 2) {
-    for (int i = 0; i < m / 2; ++i) {
-      for (int j = i; j < n; j += m) {
-        int k = j + m / 2;
-        std::complex<ld> x = res[j] - res[k];
-        res[j] += res[k];
-        res[k] = exp(i * theta * std::complex<ld>(0, 1)) * x;
+namespace {
+  using complex = std::complex<ld>;
+  std::vector<complex> FFT(const std::vector<complex> &a, int m) {
+    ld theta = 2.0 * pi / m;
+    const int n = a.size();
+    std::vector<complex> res = a;
+    for (int m = n; m >= 2; m /= 2, theta *= 2) {
+      for (int i = 0; i < m / 2; ++i) {
+        for (int j = i; j < n; j += m) {
+          int k = j + m / 2;
+          complex x = res[j] - res[k];
+          res[j] += res[k];
+          res[k] = exp(i * theta * complex(0, 1)) * x;
+        }
       }
     }
+    for (int i = 0, j = 1; j < n - 1; ++j) {
+      for (int k = n / 2; k > (i ^= k); k /= 2)
+        ;
+      if (j < i) std::swap(res[i], res[j]);
+    }
+    return res;
   }
-  for (int i = 0, j = 1; j < n - 1; ++j) {
-    for (int k = n / 2; k > (i ^= k); k /= 2)
-      ;
-    if (j < i) std::swap(res[i], res[j]);
-  }
-  return res;
 }
 
 std::vector<ll> convolution(const std::vector<ll> &lhs,
