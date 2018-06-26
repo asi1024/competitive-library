5ed0e2a9d117085c9a767aaf77823bd120386468
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2018-06-27 01:11:53 +0900

    Fix namespace format

--- cpp/include/math/fft.cpp
+++ cpp/include/math/fft.cpp
@@ -4,29 +4,29 @@
 #include "../template/typedef.hpp"
 
 namespace {
-  using complex = std::complex<ld>;
-  std::vector<complex> FFT(const std::vector<complex> &a, int m) {
-    ld theta = 2.0 * pi / m;
-    const int n = a.size();
-    std::vector<complex> res = a;
-    for (int m = n; m >= 2; m /= 2, theta *= 2) {
-      for (int i = 0; i < m / 2; ++i) {
-        for (int j = i; j < n; j += m) {
-          int k = j + m / 2;
-          complex x = res[j] - res[k];
-          res[j] += res[k];
-          res[k] = exp(i * theta * complex(0, 1)) * x;
-        }
+using complex = std::complex<ld>;
+std::vector<complex> FFT(const std::vector<complex> &a, int m) {
+  ld theta = 2.0 * pi / m;
+  const int n = a.size();
+  std::vector<complex> res = a;
+  for (int m = n; m >= 2; m /= 2, theta *= 2) {
+    for (int i = 0; i < m / 2; ++i) {
+      for (int j = i; j < n; j += m) {
+        int k = j + m / 2;
+        complex x = res[j] - res[k];
+        res[j] += res[k];
+        res[k] = exp(i * theta * complex(0, 1)) * x;
       }
     }
-    for (int i = 0, j = 1; j < n - 1; ++j) {
-      for (int k = n / 2; k > (i ^= k); k /= 2)
-        ;
-      if (j < i) std::swap(res[i], res[j]);
-    }
-    return res;
   }
+  for (int i = 0, j = 1; j < n - 1; ++j) {
+    for (int k = n / 2; k > (i ^= k); k /= 2)
+      ;
+    if (j < i) std::swap(res[i], res[j]);
+  }
+  return res;
 }
+}  // namespace
 
 std::vector<ll> convolution(const std::vector<ll> &lhs,
                             const std::vector<ll> &rhs) {
