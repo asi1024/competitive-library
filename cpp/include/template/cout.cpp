3e60005394ef9fca4001933017c502842fed56da
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2018-07-05 20:43:18 +0900

    Add cout set

--- cpp/include/template/cout.cpp
+++ cpp/include/template/cout.cpp
@@ -33,3 +33,12 @@ std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
   }
   return os;
 }
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const std::set<T> &v) {
+  for (auto it = v.begin(); it != v.end(); ++it) {
+    if (it != v.begin()) os << " ";
+    os << *it;
+  }
+  return os;
+}
