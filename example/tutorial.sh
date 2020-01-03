adaa37cb73adaef8b7faf3d061f3be4fad2b9606
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2020-01-03 22:46:43 +0900

    Show output file

--- example/tutorial.sh
+++ example/tutorial.sh
@@ -5,5 +5,5 @@ set -eux
 # Install `competitive-library` to use preprocessor.
 # $ pip install git+https://github.com/asi1024/competitive-library.git
 
-preprocessor sample.cpp > combined.cpp
+preprocessor sample.cpp | tee combined.cpp
 g++ --std=c++14 -O2 combined.cpp -o a.out
