4f6f85756d70fd136bafff04f0991b419b354481
Akifumi Imanishi <akifumi.imanishi@gmail.com>
2020-01-03 22:29:37 +0900

    Move pip-install from tutorial.sh

--- example/tutorial.sh
+++ example/tutorial.sh
@@ -2,7 +2,8 @@
 
 set -eux
 
-pip install -U pip
-pip install git+https://github.com/asi1024/competitive-library.git
+# Install `competitive-library` to use preprocessor.
+# $ pip install git+https://github.com/asi1024/competitive-library.git
+
 preprocessor sample.cpp > combined.cpp
 g++ --std=c++14 -O2 combined.cpp -o a.out
